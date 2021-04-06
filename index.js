9ed3d2a3152de61c12b51ae5be94083b02292acc
Stefan <stefan@vandockum.nl>
2021-04-06 22:48:38 +0200

    self assignable roles and basic stuff.from now on its just progging

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const config = require('./config.json');
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -9,3 +10,28 @@ client.once('ready', () => {
 });
 
 client.login(process.env.TOKEN);
+
+client.on('message', message => {
+	console.log(message.content);
+
+	const skynetrole = message.guild.roles.cache.find(role => role.name == 'Skynet');
+	if(!skynetrole) return console.log('Role doesen\'t exist.');
+
+	if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+
+	const args = message.content.slice(config.prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (command === 'skyquote') {
+		message.channel.send('Ill be back');
+	}
+	else if (command === 'skynetrole') {
+		message.channel.send('Welcome!');
+		message.member.roles.add(skynetrole);
+	}
+
+	if (args[0] === 'foo') {
+		return message.channel.send('bar');
+	}
+
+});
